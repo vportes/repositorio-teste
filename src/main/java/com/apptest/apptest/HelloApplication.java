ea55df43187894491a066a3a1bcd7200b606082d
Vinícius Portes <viniciusportescn@gmail.com>
2023-08-04 15:01:50 -0300

    Revert "Revert "Test""
    This reverts commit 6fc5a613c1a2957564dfd3489e070bfc75bf87a8.

--- src/main/java/com/apptest/apptest/HelloApplication.java
+++ src/main/java/com/apptest/apptest/HelloApplication.java
@@ -3,6 +3,7 @@ package com.apptest.apptest;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.getIcons().add(new Image("file:src/main/java/com/apptest/apptest/icon.png")); //adicionar icon ao aplicativo, necessita do import da linha 6
+        stage.setTitle("Teste de app");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/apptest/apptest/HelloController.java
+++ src/main/java/com/apptest/apptest/HelloController.java
@@ -5,10 +5,10 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label testeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onTestButtonClick() {
+        testeText.setText("Bem vindo ao aplicativo teste");
     }
 }
