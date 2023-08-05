3c680c91d46b1bbfd50d93c6c97571264e55808b
Vinícius Portes <viniciusportescn@gmail.com>
2023-08-05 18:17:53 -0300

    optimizar imports e logging

--- src/main/java/com/apptest/apptest/secondController.java
+++ src/main/java/com/apptest/apptest/secondController.java
@@ -1,20 +1,21 @@
 package com.apptest.apptest;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
 
-import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class secondController {
-    @FXML
-    private Text secondText;
+    private static final Logger logger = Logger.getLogger(secondController.class.getName()); //logger
 
-    @FXML
-    protected void onSecondButtonClick() {
+    @FXML private Text secondText;
+
+    @FXML protected void onSecondButtonClick() {
         try { // Carregar start-view.fxml
             FXMLLoader loader = new FXMLLoader(getClass().getResource("start-view.fxml"));
             Parent root = loader.load();
@@ -22,8 +23,8 @@ public class secondController {
             Stage stage = (Stage) secondText.getScene().getWindow(); // Pegar a cena em que secondText pertence (a atual)
             stage.setScene(startViewScene);
             stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "An error occurred: ", e);
         }
     }
 }
